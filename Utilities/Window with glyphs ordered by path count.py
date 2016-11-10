33a2a1944ca980f86b9d0a81e0983aeb4243b8ab
juandelperal <juandelperal@gmail.com>
2016-11-10 12:26:02 -0300

    added

---
+++ Utilities/Window with glyphs ordered by path count.py
@@ -0,0 +1,50 @@
+#MenuTitle: Window with glyphs ordered by path count
+# -*- coding: utf-8 -*-
+__doc__="""
+Window with glyphs ordered by path count. Also sum components
+"""
+
+import GlyphsApp
+
+Doc = Glyphs.currentDocument
+Font = Glyphs.font
+selectedLayers = Font.selectedLayers
+
+# Create array
+glyphs = []
+
+# Fill array
+for thisLayer in selectedLayers:
+	if thisLayer.parent.subCategory != 'Space':
+		count = len(thisLayer.paths) + len(thisLayer.components)
+		glyphs.append([ count, thisLayer.parent.name ])
+
+# Sort Glyphs by node count
+sortedGlyphs = sorted(glyphs) # order by node count
+
+# Create collection grouped by node count
+outputArray = {}
+actualCount = False
+
+for g in sortedGlyphs:
+	if actualCount == False or actualCount != g[0]:
+		# cambia el count y agrega key
+		actualCount = g[0]
+		outputArray[actualCount] = []
+
+	# Add glyph
+	outputArray[actualCount].append(g[1])
+
+# Reset
+actualCount = False
+
+# Create output
+outputString = ''
+
+for count in outputArray:
+	outputString += "\n"
+
+	for x in outputArray[count]:
+		outputString += "/" + x
+
+Doc.windowController().performSelectorOnMainThread_withObject_waitUntilDone_( "addTabWithString:", outputString, True )
