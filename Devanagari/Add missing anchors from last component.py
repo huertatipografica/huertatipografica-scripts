403a5bfeb3f50e5b8861d46020c6736f75dc86c4
juandelperal <juandelperal@gmail.com>
2016-12-12 15:02:12 -0300

    cases script

---
+++ Devanagari/Add missing anchors from last component.py
@@ -0,0 +1,51 @@
+#MenuTitle: Add missing anchors from last component
+# -*- coding: utf-8 -*-
+__doc__="""
+Copy the missing the anchors contained in the last component that has the selected glyph
+"""
+
+import GlyphsApp
+
+Doc = Glyphs.currentDocument
+Font = Glyphs.font
+FontMaster = Font.selectedFontMaster
+selectedLayers = Font.selectedLayers
+
+try:
+	Glyphs.clearLog()
+	# Glyphs.showMacroWindow()
+except:
+	pass
+
+for thisLayer in selectedLayers:
+	# Reset array
+	# components = [ component for component in thisLayer.components if Font.glyphs[ component.componentName ].category == 'Letter']
+	components = [ component for component in thisLayer.components]
+	anchors = [ anchor.name for anchor in thisLayer.anchors]
+	last = len(components) - 1
+
+	if len(components) > 0:
+		# select last component
+		componentName = thisLayer.components[last].componentName
+		componentOffsetX = thisLayer.components[last].position.x
+		componentOffsetY = thisLayer.components[last].position.y
+
+		# select glyph referenced on last component
+		componentLayer = Font.glyphs[ componentName ].layers[ FontMaster.id ]
+
+		print thisLayer.parent.name.upper()
+		# Copy all anchors
+		for anchor in componentLayer.anchors:
+
+			thisAnchorPosition = NSPoint()
+			thisAnchorPosition.x = anchor.x + componentOffsetX
+			thisAnchorPosition.y = anchor.y + componentOffsetY
+
+			thisAnchorName = anchor.name
+			thisAnchor = GSAnchor( thisAnchorName, thisAnchorPosition )
+
+			if thisAnchorName not in anchors:
+				thisLayer.addAnchor_( thisAnchor )
+				print "-- Added %s (%i, %i) from %s" % (thisAnchorName, thisAnchorPosition.x, thisAnchorPosition.y , componentName)
+			else:
+				print thisAnchorName, 'already exists in this layer'
---
+++ Utilities/New Tab with different cases.py
@@ -0,0 +1,34 @@
+#MenuTitle: New Tab with different cases
+# -*- coding: utf-8 -*-
+__doc__="""
+Opens a new edit tab with current glyphs in both cases —and small caps (configurable).
+"""
+
+import GlyphsApp
+
+thisFont = Glyphs.font # frontmost font
+thisFontMaster = thisFont.selectedFontMaster # active master
+listOfSelectedLayers = thisFont.selectedLayers # active layers of selected glyphs
+
+# Config
+separator = False # Setup separator if you want, for example 'space'
+sc = True # If you wanna include smallcaps
+
+glyphList = []
+
+for layer in listOfSelectedLayers:
+	name = layer.parent.name
+	# Uppercase
+	glyphList.append(name[0].upper() + name[1:] )
+	# Small Caps
+	if sc:
+		glyphList.append(name.lower() + ".sc")
+	# Lower
+	glyphList.append(name.lower())
+	# Separator
+	if separator:
+		glyphList.append(separator)
+
+
+tabString = "/"+"/".join(glyphList)
+thisFont.newTab(tabString)
