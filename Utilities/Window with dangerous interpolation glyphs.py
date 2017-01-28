4bc4bae66721ad63dc1057cd3688017fbc01ac01
juandelperal <juandelperal@gmail.com>
2017-01-28 11:11:08 -0300

    updated

--- Utilities/Window with dangerous interpolation glyphs.py
+++ Utilities/Window with dangerous interpolation glyphs.py
@@ -14,29 +14,29 @@ selectedLayers = Font.selectedLayers
 glyphList = []
 
 # Functions for checking
-def checkPaths( thisLayer ):
-	previousCount = 0
+def hasRepeatedPaths( thisLayer ):
+	items = []
 	for thisPath in thisLayer.paths:
-		if len(thisPath.nodes) == previousCount:
-			return True
-		previousCount = len(thisPath.nodes)
+		items.append(len(thisPath.nodes))
+	if len(items) != len(set(items)):
+		return True
 	return False
 
-def checkComponents( thisLayer ):
-	componentList = []
+def hasRepeatedComponents( thisLayer ):
+	items = []
 	for thisComponent in thisLayer.components:
-		componentList.append(thisComponent.componentName)
-	if len(componentList) != len(set(componentList)):
+		items.append(thisComponent.componentName)
+	if len(items) != len(set(items)):
 		return True
 	return False
 
 # Run the script
 for thisLayer in selectedLayers:
-	if checkPaths(thisLayer) or checkComponents(thisLayer):
+	if hasRepeatedPaths(thisLayer) or hasRepeatedComponents(thisLayer) or not thisLayer.parent.mastersCompatible:
 		glyphList.append(thisLayer.parent.name)
 
 if glyphList:
 	tabString = "/"+"/".join(glyphList)
 	Font.newTab(tabString)
 else:
-	Message("No dangerous glyphs", "There are not repeated components or paths with the same ammount of points in selected glyphs.", OKButton="Yeah")
+	Message("No dangerous glyphs", "There are not repeated components, paths with the same ammount of points or incompatible glyphs in current selection.", OKButton="Yeah")
