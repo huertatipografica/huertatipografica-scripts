528245ffdbfcb0f977dd3a39d89e25c5b1f2b2d9
juandelperal <juandelperal@gmail.com>
2017-01-31 12:59:00 -0300

    new tab scripts

---
+++ Tabs/Tab with uppercase with paths.py
@@ -0,0 +1,24 @@
+#MenuTitle: Tab with uppercase with paths
+# -*- coding: utf-8 -*-
+
+import GlyphsApp
+
+Glyphs.clearLog()
+Glyphs.showMacroWindow()
+thisFont = Glyphs.font
+selectedLayers = thisFont.selectedLayers
+
+# Scope glyphs for combine
+targetGlyphs = [g.name for g in thisFont.glyphs if g.subCategory == 'Uppercase' and len(g.layers[0].paths) > 1]
+
+lista = []
+def process (thisLayer):
+	for g in targetGlyphs:
+		lista.append(thisLayer.parent.name)
+		lista.append(g)
+
+for thisLayer in selectedLayers:
+	process (thisLayer)
+
+tabString =  "/%s" % "/".join(lista)
+thisFont.newTab( tabString )
