a3bb8185c6557302d644138bf04b3f5256d1399c
juandelperal <juandelperal@gmail.com>
2017-02-21 15:47:36 -0300

    helpers improved

--- Opentype/OpenType Helper.py
+++ Opentype/OpenType Helper.py
@@ -1,7 +1,7 @@
-#MenuTitle: Custom Parameter Helper
+#MenuTitle: OpenType Helper
 # -*- coding: utf-8 -*-
 __doc__="""
-Useful for adding Replace Glyphs and Remove Glyphs parameters. Copy and paste output in instance (Font info). Requires Vanilla
+Helps to generate OpenType replacements finding the coincidences based on given suffix.
 """
 import GlyphsApp
 import vanilla
@@ -17,7 +17,7 @@ class OpenTab(object):
 	def __init__(self):
 		self.tabString = ''
 
-		self.w = vanilla.FloatingWindow( (360, 80), "CustomParamsHelper", autosaveName="com.juandelperal.CustomParamsHelper.mainwindow" )
+		self.w = vanilla.FloatingWindow( (360, 80), "OTHelper", autosaveName="com.juandelperal.OTHelper.mainwindow" )
 		self.w.text = vanilla.TextBox( (13, 15, 320, 20), "suffix to find (ex sc)", sizeStyle='small')
 		self.w.suffix = vanilla.EditText( (15, 35, 200, 20), "H", sizeStyle='small', placeholder='ss01', callback=self.SavePreferences )
 		self.w.go = vanilla.Button( (220, 30, 60, 30), "Go", sizeStyle='small', callback=self.GoForIt )
@@ -33,13 +33,13 @@ class OpenTab(object):
 
 
 	def SavePreferences( self, sender ):
-		Glyphs.defaults["com.juandelperal.CustomParamsHelper.mainwindow.suffix"] = self.w.suffix.get()
+		Glyphs.defaults["com.juandelperal.OTHelper.mainwindow.suffix"] = self.w.suffix.get()
 
 		return True
 
 	def LoadPreferences( self ):
 		try:
-			self.w.suffix.set( Glyphs.defaults["com.juandelperal.CustomParamsHelper.mainwindow.suffix"] )
+			self.w.suffix.set( Glyphs.defaults["com.juandelperal.OTHelper.mainwindow.suffix"] )
 		except:
 			return False
 
@@ -53,36 +53,12 @@ class OpenTab(object):
 		suffix = self.w.suffix.get()
 		targetGlyphs = [g for g in glyphList if "." + suffix in g]
 
-		renameList = []
-		removeList = []
 
 		for glyph in targetGlyphs:
 			replacement = glyph.replace('.' + suffix,"")
 			if replacement in glyphList:
-				renameList.append(str("%s=%s" % ( glyph, replacement)))
-				removeList.append(str(glyph))
+				print "sub %s by %s;" % ( replacement, glyph )
 
-		# print "Replace Glyphs code\n\n"
-		# for s in renameList:
-		# 	print s
-		# print "\n\n\n\n\n\n\nRemove Glyphs code\n\n"
-		# for s in removeList:
-		# 	print s
-
-		code = (
-			{
-			'Rename Glyphs': tuple(renameList)
-			},
-			{
-			'Remove Glyphs': tuple(removeList)
-			}
-		)
-
-		# Parsing and formating entries
-		code = str(code)
-		code = code.replace(':', '=')
-		code = code.replace(')', ');')
-		print code[:-1]
 
 
 	def Close(self, sender):
