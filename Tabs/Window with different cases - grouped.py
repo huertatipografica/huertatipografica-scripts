244dd28206b71c2afa978738a8ede1ecb1e702ab
juandelperal <juandelperal@gmail.com>
2017-10-11 19:10:18 -0300

    new tab script

---
+++ Tabs/Window with different cases - grouped.py
@@ -0,0 +1,35 @@
+#MenuTitle: Window with different cases (grouped)
+# -*- coding: utf-8 -*-
+__doc__="""
+Opens a new edit tab with current glyphs in both cases —and small caps (configurable).
+"""
+
+import GlyphsApp
+
+thisFont = Glyphs.font # frontmost font
+thisFontMaster = thisFont.selectedFontMaster # active master
+listOfSelectedLayers = thisFont.selectedLayers # active layers of selected glyphs
+
+
+# Config
+sc = True # If you wanna include smallcaps
+
+upper = []
+lower = []
+sc = []
+
+for layer in listOfSelectedLayers:
+	if hasattr(layer.parent, 'name'):
+		name = layer.parent.name.replace(".sc","")
+		name = name[0].lower() + name[1:]
+
+		# Uppercase
+		upper.append(name[0].upper() + name[1:] )
+		# Small Caps
+		if sc:
+			sc.append(name.lower() + ".sc")
+		# Lower
+		lower.append(name.lower())
+
+tabString = "/"+"/".join(upper) + '\n' + "/"+"/".join(lower) + '\n' + "/"+"/".join(sc) + '\n'
+thisFont.newTab(tabString)
