f9e6b1a5d439b39bd6d3be9882393fcfcbb40721
juandelperal <juandelperal@gmail.com>
2019-11-19 19:09:12 -0300

    order parents

--- Metrics/Group parents.py
+++ Metrics/Group parents.py
@@ -8,16 +8,28 @@ import GlyphsApp
 
 thisFont = Glyphs.font # frontmost font
 selectedLayers = thisFont.selectedLayers # active layers of selected glyphs
+thisFontMaster = thisFont.selectedFontMaster # active master
 
 Glyphs.clearLog()
 
+def char(name):
+    category = thisFont[name].category
+    subCategory = thisFont[name].subCategory
+
+    return (
+        "%s%s%s" % (category, subCategory, name),
+        name,
+    )
+
 keys = set([ l.parent.leftKerningGroup for l in selectedLayers if l.parent.leftKerningGroup] + [ l.parent.rightKerningGroup for l in selectedLayers if l.parent.rightKerningGroup] )
+keys = sorted(map(char, keys))
+keys = [k[1] for k in keys]
+
 tabString = 'KerningGroups parents:\n%s\n\n' % ("/" + "/".join(keys))
 
 
 if len(keys):
 	thisFont.newTab(tabString)
-
 else:
 	Message("No kerning groups set :|", '', OKButton="OK")
 
